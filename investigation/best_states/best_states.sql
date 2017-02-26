046124f248d0e91a03ed228d27ed27dc24f2fd10
Jill Zhang <Jill Zhang>
2017-02-26 09:40:44 -0500

    final_editing

--- investigation/best_states/best_states.sql
+++
@@ -1,4 +0,0 @@
-select B.state, sum(score) as aggregate_score, avg(score) as average_score, variance(score) as variance_score from procedure A
-join hospital B on A.provider_id = B.provider_id
-group by B.state
-order by average_score desc limit 10;
--- investigation/hospital_variability/hospital_variability.sql
+++
@@ -1,4 +0,0 @@
-select A.measure_name, sum(score) as aggregate_score, avg(score) as average_score, variance(score) as variance_score from procedure A
-join hospital B on A.provider_id = B.provider_id
-group by A.measure_name
-order by variance_score desc limit 10;
